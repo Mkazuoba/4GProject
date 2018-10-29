fa91140494407459c827841b3f0925f7a0d48544
Mkazuoba <kazuomatheus02@gmail.com>
2018-10-29 14:45:37 -0300

    Add files via upload
    Index

---
+++ index.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+</head>
+
+  <body class="text-center">
+    <?php include
+    ('./includes/header2.php') ;
+        ?>
+
+      <section class="header-site text-center">
+        <h2 class="cover-heading fade-in">BEM-VINDO</h2>
+        <h1 class="cover-heading">4G SEGUROS</h1>
+
+          <br><br>
+
+        <button type="button" class="btn btn-lg btn-danger" data-toggle="modal" data-target="#exampleModal">
+          CALCULE SEU SEGURO
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade text-dark text-left" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">CALCULE SEU SEGURO</h5>
+
+              </div>
+              <div class="modal-body ">
+
+                <form>
+                  <div class="form-group ">
+                    <label for="inputName">Nome</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Seu Nome">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputEmail">Seu Email</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Seu email">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputNumber">Telefone</label>
+                    <input type="tel" class="form-control" id="inputNumber" placeholder="Telefone/ WhatsApp">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputMarca">Marca</label>
+                    <input type="tel" class="form-control" id="inputMarca" placeholder="Telefone/ WhatsApp">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputModelo">Modelo</label>
+                    <input type="tel" class="form-control" id="inputModelo" placeholder="Telefone/ WhatsApp">
+                  </div>
+                  <div class="form-group">
+                    <label for="inputAno">Ano</label>
+                    <input type="tel" class="form-control" id="inputAno" placeholder="Telefone/ WhatsApp">
+                  </div>
+                  <div class="col-auto my-1">
+                      <button type="submit" class="btn btn-outline-dark btn-lg btn-block">Solictar contato</button>
+                  </div>
+                </form>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Fechar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br><br>
+      <p class="lead">Cotação online ou Ligue agora pra gente: (11) 96739-9908</p>
+      </section>
+
+      <section class="security-site" id="security">
+              <div class="col-xs-12">
+                <h1 class="cover-heading" >SEGUROS</h1>
+                <p class="lead text-center">Trabalhamos com as melhores seguradoras desde 1997:​</p>
+              </div>
+
+          <div class="container marketing">
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/01.jpg">
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/02.jpg">
+                </div>
+              </div>
+                        <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/03.jpg">
+                </div>
+              </div>
+                      <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/04.jpg">
+                </div>
+              </div>
+                          <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/05.jpg">
+                </div>
+              </div>
+                          <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/06.jpg">
+                </div>
+              </div>
+                      <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/07.jpg">
+                </div>
+              </div>
+                        <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/08.jpg">
+                </div>
+              </div>
+                          <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/09.jpg">
+                </div>
+              </div>
+                      <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/010.jpg">
+                </div>
+              </div>
+                          <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/011.jpg">
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="thumbnail">
+                  <img src="img/seguros/012.jpg">
+                </div>
+              </div>
+            </div>
+
+
+      </section>
+
+      <section class="covers-site " id="covers">
+        <div class="container marketing">
+        <h1 class="cover-heading">COBERTURA DO SEGURO AUTO</h1>
+        <br><br>
+
+        <section class="cover-img">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="img/icons/accident.png">
+                  <h4>Batidas Roubo</h4>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="img/icons/car1.png">
+                  <h4>Incêndio</h4>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="img/icons/trailer1.png">
+                  <h4>Assistência Guincho 24 horas</h4>
+              </div>
+            </div>
+<div class="container ">
+  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Mostrar todos
+</button>
+
+<!-- Modal -->
+<div class="modal fade text-dark text-left" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">COBERTURA DO SEGURO AUTO</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <p>Batidas</p>
+      <p>Roubo e furto</p>
+      <p>Incêndio/p>
+      <p>Enchentes</p>
+      <p>Assistência Guincho 24 horas</p>
+      <p>Carro Reserva</p>
+      <p>Cobertura de Vidros</p>
+      <p>Danos corporais a terceiros (Proteção para pessoas que você feriu)</p>
+      <p>Danos materiais a terceiros (Cobertura dos estragos do veículo que você bateu)</p>
+      <p>Acidentes a passageiros (Os custos por morte/invalidez dos passageiros do seu veículo)</p>
+
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Fechar</button>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+          </section>
+
+
+
+      </section>
+
+      <section class="services-site" id="services">
+        <h1 class="cover-heading">POR QUE A 4G SEGUROS?</h1>
+        <p class="lead text-center">
+
+          <div class="container marketing"><br>
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+              <div class="col-lg-3">
+                <img class="rounded-circle" src="./img/icons/004-speed.png" alt="Generic placeholder image" width="140" height="140">
+                <h4>RÁPIDEZ</h4>
+                <p>Cote online e seja atendido com rapidez.</p>
+
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-3">
+                <img class="rounded-circle" src="./img/icons/headset.png" alt="Generic placeholder image" width="140" height="140">
+                <h4>ATENDIMENTO HUMANO</h4>
+                <p>Aqui você fala com gente de verdade.</p>
+
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-3">
+                <img class="rounded-circle" src="./img/icons/sign.png" alt="Generic placeholder image" width="140" height="140">
+                <h4>TENHA OPÇÕES</h4>
+                <p>Escolha entre as 18 principais seguradoras.</p>
+
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-3">
+                <img class="rounded-circle" src="./img/icons/settings.png" alt="Generic placeholder image" width="140" height="140">
+                <h4>CUSTOMIZE</h4>
+                <p>Seguro que atende as suas necessidades</p>
+
+              </div><!-- /.col-lg-4 -->
+            </div><!-- /.row --></section>
+
+      <section class="location container" id="where">
+        <div class="text-center">
+          <h1>COMO CHEGAR NA 4G</h1>
+          <p>Av. Mario Lopes Leão, 1500 Sala 306 - Santo Amaro</p>
+      </div>
+
+        <div class="google-maps">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.6358690390084!2d-46.72274188502058!3d-23.65320808463741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce51083dbe2bdd%3A0xe0eba59bd430070a!2sAvenida+M%C3%A1rio+Lopes+Le%C3%A3o%2C+1500+-+306+-+Santo+Amaro%2C+S%C3%A3o+Paulo+-+SP%2C+04753-100!5e0!3m2!1spt-BR!2sbr!4v1538691329045" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
+
+</section>
+
+<section class="footer-color">
+      <?php include
+      ('./includes/footer2.php') ;
+          ?>
+</sction>
+
+  </body>
+</html>
